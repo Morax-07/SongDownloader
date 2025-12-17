fffbc36b23bcbe0540dc0b5fdf6db742fdddcbe8
Morax-07 <francesco.07.mora@gmail.com>
2025-12-17 08:19:18 +0100

    Script finito

--- download_mp3.sh
+++ download_mp3.sh
@@ -2,18 +2,20 @@
 
 
 
+mkdir -p download
+
 while IFS= read -r linea || [[ -n "$linea" ]]; do
     titolo=$(echo "$linea" | cut -d"," -f1)
     link=$(echo "$linea" | cut -d"," -f2)
 
-    # Use correctly formatted cookie file
-    yt-dlp --cookies-from-browser chrome -x --audio-format mp3 "$link" -o "download/$titolo.mp3"
 
-    estensione=$(echo "$titolo.*" | sed 's/.*\.//')
+    index=$(echo "$link" | sed -n 's/.*[?&]index=\([0-9]\+\).*/\1/p')
 
-    if command -v ffmpeg &> /dev/null; then
-        ffmpeg -i "$titolo.$estensione" -q:a 0 "$titolo.mp3"
+
+    if [[ -n "$index" ]]; then
+	yt-dlp --playlist-items "$index" --cookies-from-browser chrome -x --audio-format mp3 "$link" -o "download/$titolo.mp3"	
     else
-        echo "ffmpeg non è installato. Impossibile convertire in MP3."
+        yt-dlp --playlist-items 1 --cookies-from-browser chrome -x --audio-format mp3 "$link" -o "download/$titolo.mp3"
     fi
-done < $1
+
+done < "$1"
